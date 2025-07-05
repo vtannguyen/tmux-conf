adcd38fbee891235bf0e352ace6f6b45c35788ed
Tan Nguyen <tantit91@gmail.com>
2025-07-05 16:08:44 +0200

    add activity watch plugin

--- install.sh
+++ install.sh
@@ -11,3 +11,26 @@ mv .tmux.conf ~/.tmux.conf
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CommitMono.zip
 unzip -n CommitMono.zip -d ~/.local/share/fonts
 rm CommitMono.zip
+
+# Install activitywatch
+wget https://github.com/ActivityWatch/activitywatch/releases/download/v0.13.2/activitywatch-v0.13.2-linux-x86_64.zip
+unzip activitywatch-v0.13.2-linux-x86_64.zip -d ~/.local/share
+ln -s ~/.local/share/activitywatch/aw-qt ~/.local/bin/aw-qt
+mkdir -p ~/.config/systemd/user
+
+cat <<EOF > "$HOME/.config/systemd/user/activitywatch.service"
+[Unit]
+Description=Autostart user service for activitywatch
+After=default.target
+
+[Service]
+Type=simple
+ExecStart=$HOME/.local/bin/aw-qt
+Restart=on-failure
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl --user daemon-reload
+systemctl --user enable --now "activitywatch.service"
